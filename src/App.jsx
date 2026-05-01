6b7ca762332c532c41c3119f12e6da6147a44b87
Bruno Castilho <bs-castilho@hotmail.com>
2026-04-30 21:35:10 -0300

    feat:App e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Dashboard from './pages/Dashboard'
+import Missions from './pages/Missions'
+import Profile from './pages/Profile'
+import Store from './pages/Store'
+import { AvatarProvider } from './context/AvatarContext'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    
+    <AvatarProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/missoes" element={<Missions />} />
+            <Route path="/loja" element={<Store />} />
+            <Route path="/perfil" element={<Profile />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AvatarProvider>
   )
 }
-
-export default App
